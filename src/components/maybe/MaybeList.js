69aa03f1eb2789cae8e0290e7f3bd436f470a397
mateus <mateusvanhalen@gmail.com>
2019-01-12 07:20:57 -0600

    maybes fix

--- src/components/maybe/MaybeList.js
+++ src/components/maybe/MaybeList.js
@@ -7,7 +7,11 @@ class MaybeList extends Component {
     render() {
         return (<React.Fragment>
             <div className="maybeButton">
-                <button type="image" src="sunshine.jpg"
+            <p>
+            Where is your "Maybe one day" trip? Think about it for a moment and add below!
+            </p>
+            <br/>
+                <button
                         className="btn btn-info"
                             onClick={() => {
                             this.props.history.push("/maybes/new")
@@ -15,26 +19,27 @@ class MaybeList extends Component {
                     Add a "Maybe"
                 </button>
                 <br/><br/><br/>
+                <p>See your other "Mayebes" below</p>
+                <br/><br/><br/>
             <section className="maybes">
             <br/>
             {
                 //sort by date?
                 // it is easier to sort by server call
-
                 this.props.maybes.map(maybes =>
                     <div key={maybes.id} className="card">
-                        <div className="maybe-body">
-                            <h4 className="card-title" >
-                            {"Where:"}<br/><br/>
+                        <div className="may">
+                            <h4 className="card-titlebox" >
+                            {"Where to:"}<br/><br/>
                                 {maybes.location}
                                 {/* {maybes.description} */}
                                 <br/><br/>
-                                </h4><h6>
-                            {"Why:  "}
+                            {"What for:  "}
+
                                 {maybes.why}<br/><br/>
                                     <Link className="btn btn-info" to={`/maybes/${maybes.id}`}
                             >Delete this trip idear</Link>
-                            </h6>
+                            </h4>
                         </div>
                     </div>
                 )}
